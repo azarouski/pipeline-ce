225379d4ca70f142eb41ee2c02b6261024a64631
vdelendik <vadim.delendik@gmail.com>
2021-08-25 18:44:30 +0300

    one more hotfix

--- src/com/zebrunner/jenkins/pipeline/runner/maven/TestNG.groovy
+++ src/com/zebrunner/jenkins/pipeline/runner/maven/TestNG.groovy
@@ -615,8 +615,8 @@ public class TestNG extends Runner {
             // Ignore maven build result if Zafira integration is enabled
             zafiraGoals = "-Dmaven.test.failure.ignore=true \
                             -Dzafira_enabled=true \
-                            -Dzafira_service_url=\"https://tr.zebrunner.org\" \
-                            -Dzafira_access_token="FwbrH7i8jC3wNfhczXQwSsiaOV5999EsccdweorkXbnJ9VT6WR" \
+                            -Dzafira_service_url=https://tr.zebrunner.org \
+                            -Dzafira_access_token=FwbrH7i8jC3wNfhczXQwSsiaOV5999EsccdweorkXbnJ9VT6WR \
                             -Dreporting.enabled=true \
                             -Dreporting.server.hostname=https://tr.zebrunner.org \
                             -Dreporting.server.accessToken=FwbrH7i8jC3wNfhczXQwSsiaOV5999EsccdweorkXbnJ9VT6WR \
